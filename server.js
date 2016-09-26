caf667bbc6eb5fd1db47cb0704b7289bd14d6ee9
Balajimude <balajimude36@gmail.com>
2016-09-26 16:37:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article-one={
+    title:'article-one Balaji Mude',
+    heading:'Article-one',
+    date:s'ep 5 2016',
+    content:'<p>
+                This is first content First Article
+             </p>
+             <p>Cloud computing is a type of Internet-based computing that provides shared computer processing resources and data to computers and other devices on demand. It is a model for enabling ubiquitous, on-demand access to a shared pool of configurable computing resources (e.g., computer networks, servers, storage, applications and services),[1][2] which can be rapidly provisioned and released with minimal management effort.
+             </p>'
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
